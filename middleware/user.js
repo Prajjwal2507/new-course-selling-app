70a27550d57b2088365298aa8446869149d427f6
prajjwal2507 <prajjwalrajput2507@gmail.com>
2025-09-20 23:27:47 +0530

    commit course selling backend

---
+++ middleware/user.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
+function userMiddleware(req, res, next) {
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const token = req.headers.authorization;
+  const words = tokens.split(" ");
+  const jwtToken = words[1];
+  try {
+    const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+    if (decodedValue.username) {
+      //         When you set something on req (like req.username), it travels with the request into your route handler.
+      // So inside any route that uses userMiddleware, you can directly access it.
+      // ✅ You are attaching username to request
+      req.username = decodedValue.username;
+      req.randomData = "Adsadsadsadssd";
+      next();
+    } else {
+      res.status(403).json({
+        msg: "you are not authenticated",
+      });
+    }
+  } catch (e) {
+    res.json({
+      msg: "Incorrect inputes",
+    });
+  }
+}
+
+module.exports = userMiddleware;
---
+++ routes/user.js
@@ -0,0 +1,100 @@
+const { Router } = require("express");
+const router = Router();
+const userMiddleware = require("../middleware/user");
+const { Admin, User, Course } = require("../db");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
+// User Routes
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.status(400).json({ message: "Admin already exists" });
+  }
+  User.create({
+    username,
+    password,
+  });
+  res.json({
+    message: "User created successfully",
+  });
+});
+
+router.post("/signin", async (req, res) => {
+  // Implement admin signup logic
+      const username = req.body.username;
+      const password = req.body.password;
+      console.log(JWT_SECRET);
+  
+      const user = await User.find({
+          username,
+          password
+      })
+      if (user) {
+          const token = jwt.sign({
+              username
+          }, JWT_SECRET);
+  
+          res.json({
+              token
+          })
+      } else {
+          res.status(411).json({
+              message: "Incorrect email and pass"
+          })
+      }
+});
+
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+   const response = await Course.find({});
+  
+       res.json({
+           courses: response
+       })
+});
+
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+
+    const courseId = req.params.courseId;
+      const username = req.username; // comming from middleware
+      //const randomdata = req.randomData;
+    
+      // we are not taking username in header this time , we are extracting it from jwt
+      // const username = req.headers.username;
+
+    await User.updateOne({
+        username: username
+    }, {
+        "$push": {
+            purchasedCourses: courseId
+        }
+    })
+    res.json({
+        message: "Purchase complete!"
+    })
+});
+
+router.get("/purchasedCourses", userMiddleware, async(req, res) => {
+  // Implement fetching purchased courses logic
+   const user = await User.findOne({
+        username: req.username     // comming from middleware
+    });
+
+    console.log(user.purchasedCourses);
+    const courses = await Course.find({
+        _id: {
+            "$in": user.purchasedCourses
+        }
+    });
+
+    res.json({
+        courses: courses
+    })
+});
+
+module.exports = router;
