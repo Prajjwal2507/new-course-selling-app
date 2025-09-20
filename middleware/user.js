59d79dee4d59a63e601a0b2f339b05f0b87ee095
prajjwal2507 <prajjwalrajput2507@gmail.com>
2025-09-21 00:18:00 +0530

    commit changes in middlewares

--- middleware/user.js
+++ middleware/user.js
@@ -5,7 +5,7 @@ function userMiddleware(req, res, next) {
   // Implement user auth logic
   // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
   const token = req.headers.authorization;
-  const words = tokens.split(" ");
+  const words = token.split(" ");
   const jwtToken = words[1];
   try {
     const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
